ae25a61ab7ff8171b13a4ff7dc0c684275821236
Sean Abbas <sean.abbas33@gmail.com>
2023-03-21 15:38:39 -0400

    weather

--- practice-for-week-15-react-refactoring-class-components-long-practice-main/src/App.js
+++ practice-for-week-15-react-refactoring-class-components-long-practice-main/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Clock, { ClockToggle } from './components/Clock';
 import Folder from './components/Folder';
 import Weather from './components/Weather';
@@ -21,27 +21,23 @@ const folders = [
   { title: 'three', content: 'Third folder here' }
 ];
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showClock: true
-    };
+function App() {
+  const [showClock, setShowClock] = useState(true);
+  
+  // toggleClock = () => this.setState({ showClock: !this.state.showClock });
+
+  const toggleClock = () => {
+    setShowClock(oldShowClock => !oldShowClock)
   }
-  
-  toggleClock = () => this.setState({ showClock: !this.state.showClock });
-  
-  render () {
     return (
       <div className="widgets">
         <Folder folders={folders} />
         <Weather />
-        <ClockToggle toggleClock={this.toggleClock} />
-        {this.state.showClock && <Clock />}
+        <ClockToggle toggleClock={toggleClock} />
+        {showClock && <Clock />}
         <Autocomplete names={names} />
       </div>
     );
-  }
 }
 
 export default App;
--- practice-for-week-15-react-refactoring-class-components-long-practice-main/src/components/Folder.js
+++ practice-for-week-15-react-refactoring-class-components-long-practice-main/src/components/Folder.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Headers = ({ titles, currentTab, selectTab }) => {
   const handleClick = (e) => {
@@ -28,38 +28,34 @@ const Headers = ({ titles, currentTab, selectTab }) => {
   );
 }
 
-class Folder extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentTab: 0
-    };
-  }
+function Folder(props) {
+  const [currentTab, setCurrentTab] = useState(0);
   
-  selectTab = (num) => {
-    this.setState({ currentTab: num });
+  const selectTab = (num) => {
+    setCurrentTab(num);
   }
+
+  const folder = props.folders[currentTab];
+  const titles = props.folders.map((folder) => folder.title);
   
-  render() {
-    const folder = this.props.folders[this.state.currentTab];
-    const titles = this.props.folders.map((folder) => folder.title);
+
     
-    return (
-      <section className="tabs-section">
-        <h1>Tabs</h1>
-        <div className='tabs'>
-          <Headers
-            titles={titles}
-            currentTab={this.state.currentTab}
-            selectTab={this.selectTab}
-          />
-          <div className='tab-content'>
-            {folder.content}
-          </div>
+  return (
+    <section className="tabs-section">
+      <h1>Tabs</h1>
+      <div className='tabs'>
+        <Headers
+          titles={titles}
+          currentTab={currentTab}
+          selectTab={selectTab}
+        />
+        <div className='tab-content'>
+          {folder.content}
         </div>
-      </section>
-    );
-  }
+      </div>
+    </section>
+  );
 }
+
 
 export default Folder;
